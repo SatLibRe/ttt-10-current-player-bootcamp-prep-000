3a1f6bd9f6ea7d3e07518f8d644158a29bf4eed3
Miles Higbie <miles.higbie@gmail.com>
2020-01-03 01:16:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,5 +9,9 @@ def turn_count(board1)
 end 
 
 def current_player(board)
-  
+    if turn_count(board1) == 0 || turn_count(board1) == 3
+      return "X"
+    elsif turn_count(board1) == 2
+      return "O"
+  end 
 end
